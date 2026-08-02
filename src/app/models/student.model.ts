d99f58b0de387cf2e008f37d4978d6dc18d6b153
SeifEmad <59242965+SeiifEmad@users.noreply.github.com>
2026-08-02 15:16:45 +0300

    Update student.model.ts

--- src/app/models/student.model.ts
+++ src/app/models/student.model.ts
@@ -1,9 +1,7 @@
-// موديل الطالب (Student Model)
-// بيوصف شكل البيانات بتاعة كل طالب في النظام
 export interface Student {
   id: number;
   name: string;
   age: number;
   grade: number;
-  status: 'Passed' | 'Failed'; // بيتحسب تلقائي من الـ grade
+  status: 'Passed' | 'Failed'; 
 }
